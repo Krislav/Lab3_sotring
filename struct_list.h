895788c3b311019abde7d3f612055790aea9b6b2
Krislav <kirillsokolyanki@gmail.com>
2024-12-05 12:55:44 +0300

    Дообавлена структура хранения данных

--- struct_list.h
+++ struct_list.h
@@ -8,10 +8,20 @@ typedef struct elem{
     struct elem *next;
 } elem; //Элемент двухсвязного списка
 typedef struct {
-
+    elem *head;
+    elem *tail;
+    long size;
 } list; //Двухсвязный список
 typedef struct {
-
+    char name[128];
+    char surname[128];
+    char initials[2];
+    char magazine_name[128];
+    int year;
+    int volume;
+    bool include_in_RINC;
+    int number_of_pages;
+    int citations;
 } inf; //Структура для данных
 
 #endif
